6044febfd5597f837ec9527900343d9fa2df1ea8
Dmitry Molokhov <molokhov@outlook.com>
2020-06-12 14:06:57 +0800

    Parsing compositions for one or many sheets

--- corelab_reader.py
+++ corelab_reader.py
@@ -12,8 +12,11 @@ import re
 # Class to store sample data and relevant fluid properties.
 class FlashExperimentData:
     
-    def __init__(self, composition, ave_MW):
-        pass
+    def __init__(self, lqd, gas, res, ave_lqd_mw):
+        self.liquid = lqd
+        self.gas = gas
+        self.res = res
+        self.av_lqd_mw = ave_lqd_mw
     
     def assign_composition(self):
         pass
@@ -26,11 +29,16 @@ class FlashExperimentData:
 # openpyxl only works with Excel 2010+ file format.
 class CoreLabsXLSXLoader:
     
-    def __init__(self, report_path):
+    # Depending whether a specific worksheet is input a class instance has
+    # a functionality either to read the whole flash data found in the
+    # workbook or just that individual worksheet.
+    def __init__(self, report_path, ws=None):
         self.file_path = report_path
+        self.worksheet = ws
     
-    def read_flash_data(self):
-        worksheet = 'C.1'
+    # Method to parse an individual worksheet.
+    def read_flash_data(self, ws):
+        worksheet = ws
         # Reading the composition table as the whole.
         df = pd.read_excel(self.file_path, worksheet, skiprows = 11, nrows = 52, 
                         usecols = 'B:I', header = None,
@@ -38,24 +46,55 @@ class CoreLabsXLSXLoader:
                                  'gas_wp', 'res_mp', 'res_wp'], na_values = ' ')
         # Filling in empty cells in carbon group columns.
         df['scn'] = df['scn'].ffill()
-        print(df)
+        df.set_index(['scn', 'cl_name'], inplace=True)
+        liq = df.iloc[:, 0:2]
+        gas = df.iloc[:, 2:4]
+        res = df.iloc[:, 4:]
+        return liq, gas, res
     
     def read(self):
         wb = load_workbook(self.file_path)
-        flash_data_list = [worksheet for worksheet in wb.sheetnames if
-                           re.search('C\.\d+', worksheet)]
-        print(flash_data_list)
-        # pass
+        if self.worksheet is None:
+            flash_data_list = [worksheet for worksheet in wb.sheetnames if
+                               re.search('C\.\d+', worksheet)]
+        else:
+            flash_data_list = [self.worksheet]
+        for worksheet in flash_data_list:
+            sheet = wb[worksheet]
+            desc = sheet['B8'].value+sheet['B9'].value
+            depth, sample_num, cylinder = self.__parser(desc)
+            print('Depth: ', depth, 'Sample number: ', sample_num, 'Cylinder: ', cylinder)
+            liq, gas, res = self.read_flash_data(worksheet)
+            # Typically, flashed liquid average mole weight in CL reports is in
+            # the cell O39:
+            lqd_av_mw = sheet['B8'].value+sheet['O39'].value
+            
+        print(liq)
+    
+    # Parcer function is supposed to extract useful sample descriptors from
+    # the text strings above the composition table. In most Core LAbs reports
+    # these strings are in the cells B8 and B9. Presumably, they contain
+    # sample depth, cylinder and sample numbers. At this stage I am only
+    # extracting numerical part of the these numbers. Depth is assumed to be
+    # MD depth in metres.
+    def __parser(self, descript_string):
+        try:
+            depth = float(re.search('Depth\D+(\d+\.?\d*)', descript_string).group(1))
+        except:
+            depth = None
+        try:
+            sample_num = re.search('Sample N\D+(\d+\.?\d*)', descript_string).group(1)
+        except:
+            sample_num = 'N/A'
+        try:
+            cylinder = re.search('(Cylinder|Chamber)\D+(\d+)', descript_string).group(2)
+        except:
+            cylinder = 'N/A'
+        return depth, sample_num, cylinder
     
 if __name__ == "__main__":
     
     path = '..\..\PVT_Reports\PS1.xlsx'
-    cl_report = CoreLabsXLSXLoader(path)
-    # cl_report.read()
-    cl_report.read_flash_data()
-
-# ws = wb['C.1']
-
-# cell_range = ws['B12':'C63']
-
-# print(cell_range)
+    cl_report = CoreLabsXLSXLoader(path, ws='C.1')
+    cl_report.read()
+    # cl_report.read_flash_data()
