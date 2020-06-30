0f65b9e2fdbecac3dffeee060d9628da934d8037
Dmitry Molokhov <molokhov@outlook.com>
2020-06-30 15:23:42 +0800

    Created regression variables array

--- corelab_reader.py
+++ corelab_reader.py
@@ -25,6 +25,7 @@ class FlashExperimentData:
         self._ave_C7_mw = None
         self._c10_heavy_end_lqd = None
         self._c7_heavy_end_lqd = None
+        self.gamma_input = None
     
     def assign_composition(self):
         pass
@@ -49,67 +50,76 @@ class FlashExperimentData:
         i = self.liquid[self.liquid['scn'] == 'C7'].index[0]
         self._c7_heavy_end_lqd = self.liquid.iloc[i:]
         return self._c7_heavy_end_lqd
+    
+    def prepare_input(self, id, n=10):
+        lqd=self.c10_heavy_end_lqd
+        mw=self.av_lqd_mw
+        self.gamma_input = lqd.copy()
+        self.gamma_input['MWi_lab'] = self.gamma_input.apply(lambda x : x['lqd_wp']*mw/x['lqd_mp'], axis = 1)
+        # Calculating initial values of upper bounds of individual MWn slices.
+        # Upper bounds are considered as midway between SCN MW values.
+        # C36+ upper bound is set to an arbitrary number (10000).
+        self.gamma_input['ubound_init'] = self.gamma_input['MWi_lab']+(self.gamma_input['MWi_lab'].shift(-1)-self.gamma_input['MWi_lab'])/2
+        self.gamma_input['ubound_init'] = self.gamma_input['ubound_init'].fillna(100000)
+        
+        # Generating regresion variables for component molecular weight bounds:
+        self.gamma_input['ubound'] = 'm'+self.gamma_input['scn']
+        
+        # Adding top row to represent a lower boundary of C10 (or upper C9 boundary)
+        top_index = 'C'+str(n-1)
+        df_top = pd.DataFrame(pd.DataFrame([[top_index]+[np.nan] * (len(self.gamma_input.columns)-1)], columns=self.gamma_input.columns))
+        self.gamma_input = df_top.append(self.gamma_input, ignore_index=True)
+        
+        # Calculating rescaled C10+ weight fractions.
+        self.gamma_input['wni_lab'] = self.gamma_input['lqd_wp']/self.gamma_input['lqd_wp'].sum()
+        
+        # Adding a regression variable for lower C10 molecular weight boundary:
+        self.gamma_input.at[0,'ubound'] = 'ita'
+        self.gamma_input.iloc[-1, self.gamma_input.columns.get_loc('ubound')] = self.gamma_input.iloc[-1, self.gamma_input.columns.get_loc('ubound_init')]
+        self.gamma_input['sample_id'] = id.replace('.', '_')
+        self.gamma_input['heavy_end_mw'] = id.replace('.', '_')+'_heavy_mw'
+
 
 # Class to store multiple sample data.
 class FlashExpDataCollection(FlashExperimentData, dict):
     
     def __init__(self, worksheet_names):
         dict.__init__(self, ((wn, FlashExperimentData.__init__(self)) for wn in worksheet_names))
-        self._collection = []
         
     @property
     def sample_names(self):
-        return self.keys()
+        return list(self.keys())
     
     def add_sample(self, sample_name, sample):
         self[sample_name] = sample
         
-    def _prepare_input(self, lqd, mw, id, n=10):
-        df = lqd.copy()
-        df['MWi_lab'] = df.apply(lambda x : x['lqd_wp']*mw/x['lqd_mp'], axis = 1)
-        # Calculating initial values of upper bounds of individual MWn slices.
-        # Upper bounds are considered as midway between SCN MW values.
-        # C36+ upper bound is set to an arbitrary number (10000).
-        df['ubound_init'] = df['MWi_lab']+(df['MWi_lab'].shift(-1)-df['MWi_lab'])/2
-        df['ubound_init'] = df['ubound_init'].fillna(100000)
-        
-        # Generating regresion variables for component molecular weight bounds:
-        df['ubound'] = 'm'+df['scn']
-        
-        # Adding top row to represent a lower boundary of C10 (or upper C9 boundary)
-        top_index = 'C'+str(n-1)
-        df_top = pd.DataFrame(pd.DataFrame([[top_index]+[np.nan] * (len(df.columns)-1)], columns=df.columns))
-        df = df_top.append(df, ignore_index=True)
-        
-        # Calculating rescaled C10+ weight fractions.
-        df['wni_lab'] = df['lqd_wp']/df['lqd_wp'].sum()
-        
-        # Adding a regression variable for lower C10 molecular weight boundary:
-        df.at[0,'ubound'] = 'ita'
-        df.iloc[-1, df.columns.get_loc('ubound')] = df.iloc[-1, df.columns.get_loc('ubound_init')]
-        df['sample_id'] = id.replace('.', '_')
-        df['heavy_end_mw'] = id.replace('.', '_')+'_heavy_mw'
-        return df
-        
     def _prepare_regression(self):
         for key, item in self.items():
-            df = self._prepare_input(item.c10_heavy_end_lqd, item.av_lqd_mw, key)
-            self._collection.append(df)
+            item.prepare_input(key)
+            # df = self._prepare_input(item.c10_heavy_end_lqd, item.av_lqd_mw, key)
+            # self._collection.append(df)
         
-    def gamma_distribution_fit(self, n=10):
+    def gamma_distribution_fit(self, n=10, alpha=1):
         # Gamma distribution fit from C7 is not implemented yet.
         if n == 7:
             raise NotImplementedError
         self._prepare_regression()
-        # reg_variables = np.concatenate(self._fit_df.loc[self._fit_df.index[0:-1], 'ubound'].unique(),#, np.array(['alpha']), 
-        #                                 self._fit_df.loc[self._fit_df.index[0:-1], 'heavy_end_mw'].unique())
-        reg_variables = np.concatenate((np.array(['alpha']), self._fit_df.loc[self._fit_df.index[0:-1], 'ubound'].unique(),
-                                        self._fit_df.loc[self._fit_df.index[0:-1], 'heavy_end_mw'].unique()))
-        reg_variables = np.delete(reg_variables, np.where(reg_variables == 100000))
-        init_vals = np.concatenate((np.array([1.0]), self._fit_df.loc[self._fit_df.index[0:-1].unique(), 'ubound_init']))
+        # reg_variables = np.concatenate(( 
+        #                                 self[sample_names[0]].gamma_input.loc[self[sample_names[0]].gamma_input.index[0:-1], 'ubound'].unique())#,
+                                        # self[sample_names[0]].gamma_input.loc[self._fit_df.index[0:-1], 'heavy_end_mw'].unique()))
+        # reg_variables = np.delete(reg_variables, np.where(reg_variables == 100000))
+        df = self[self.sample_names[0]].gamma_input
+        reg_variables = np.concatenate((np.array(['alpha']), df.loc[df.index[0:-1], 'ubound'].unique(),
+                                        np.array([sample.replace('.', '_')+'_heavy_mw' for sample in self.sample_names])))
+        init_vals = df.loc[df.index[0:-1], 'ubound_init']
+        init_vals.iloc[0] = init_vals.iloc[1]-14
+        init_vals = pd.Series(alpha).append(init_vals, ignore_index=True)
+        for key, item in self.items():
+            init_vals = init_vals.append(pd.Series(item.ave_C10_mw), ignore_index=True)
+        
+        print(reg_variables)
         print(init_vals)
-        # print(self._fit_df.head())
-        # self._fit_df.to_csv('out.csv')
+
 
 # Class that contains functionality to parse a Core Labs Excel report
 # and pass the data to FlashExperimentData class instance.
